ea0a8217778a0f5fe76e0ba2db05f0cf2974ec6e
Joe <joseph_a_f@hotmail.com>
2026-03-22 08:43:11 -0700

    Added custom look

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,26 @@ function App() {
           </p>
         </div>
       </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <h2>Network Architecture and Cloud Computing - D782</h2>
+          <h2>Task 2: Network Security</h2>
+          <h2>Joseph Farrish</h2>
+          <p>This is a Basic MERN app for testing of Microsoft Azure services.</p>
+          <ul>
+            <li>
+              <a href="https://github.com/joejoe909/CloudCampusIQ_D782" target="_blank">
+                Git Hub Repo
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
       <section id="spacer"></section>
     </>
   )
